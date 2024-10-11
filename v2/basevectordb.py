2b6900d5e610c2084fa0f09784fe4b2618517ff5
tikendraw <tikendraksahu1029@gmail.com>
2024-10-11 21:26:55 +0530

    base vector class

---
+++ v2/basevectordb.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+
+class BaseVectorDB(ABC):
+    
+
+
+    @abstractmethod
+    def get_n_similar_images(self, x:list[float], k:int=5) -> list[float]:
+        pass
+    
+    @abstractmethod
+    def add_images(self, images:list) -> None:
+        pass
+    

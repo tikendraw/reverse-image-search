43cd73256f48e33aa87b5ff84576a40d1033a058
tikendraw <tikendraksahu1029@gmail.com>
2024-10-13 19:00:59 +0530

    s

--- v2/embed_model.py
+++ v2/embed_model.py
@@ -1,10 +1,10 @@
-import matplotlib.pyplot as plt
 from pathlib import Path
-from transformers import AutoImageProcessor, EfficientNetModel
+from transformers import AutoImageProcessor, AutoModel
 from chromadb.api.types import Documents, EmbeddingFunction, Embeddings
 from PIL import Image
 import torch
 import numpy as np
+
 
 class EfficientNetEmbeddingFunction(EmbeddingFunction[Documents]):
     """To use this EmbeddingFunction, you must have the google.generativeai Python package installed and have a PaLM API key."""
@@ -15,23 +15,21 @@ class EfficientNetEmbeddingFunction(EmbeddingFunction[Documents]):
         )
         self._model_name = model_name
         self.image_processor = AutoImageProcessor.from_pretrained(model_name)
-        self.model = EfficientNetModel.from_pretrained(model_name).to(self.device)
+        # self.model = EfficientNetModel.from_pretrained(model_name).to(self.device)
+        self.model = AutoModel.from_pretrained(model_name).to(self.device)
 
     def load_image(self, image: str | Path | Image.Image | np.ndarray):
         """
         Loads an image and processes it using the model's image processor.
         Reusable helper function for single and batch image processing.
         """
-        
-        
+
         if isinstance(image, str) or isinstance(image, Path):
             image = Image.open(image)
-        
 
-        inputs = self.image_processor(images=image, return_tensors="pt").to(self.device)
-        # print('piexel: ',inputs['pixel_values'].shape)
-        # print('type: ',type(inputs['pixel_values']))
-        return inputs['pixel_values']
+        image = self.image_processor(images=image, return_tensors="pt").to(self.device)
+
+        return image["pixel_values"]
 
     def _embed(self, pixel_values):
         """
@@ -49,14 +47,16 @@ class EfficientNetEmbeddingFunction(EmbeddingFunction[Documents]):
         embeddings = self._embed(pixel_values)
         return embeddings.cpu().numpy().tolist()
 
-    def batch_embed_images(self, images: list[str | Path | Image.Image], batch_size: int = 32):
+    def batch_embed_images(
+        self, images: list[str | Path | Image.Image | np.ndarray], batch_size: int = 32
+    ):
         """
         Embeds a batch of images, processing them in batches of the specified batch_size.
         """
         all_embeddings = []
 
         for i in range(0, len(images), batch_size):
-            batch = images[i:i + batch_size]
+            batch = images[i : i + batch_size]
             processed_images = [self.load_image(image) for image in batch]
             batched_images = torch.cat(processed_images).to(self.device)
             batch_embeddings = self._embed(batched_images)
@@ -64,5 +64,7 @@ class EfficientNetEmbeddingFunction(EmbeddingFunction[Documents]):
 
         return all_embeddings
 
-    def __call__(self, images: list[Image.Image | Path | str], batch_size: int = 32) -> Embeddings:
+    def __call__(
+        self, images: list[Image.Image | Path | str], batch_size: int = 8
+    ) -> Embeddings:
         return self.batch_embed_images(images, batch_size=batch_size)

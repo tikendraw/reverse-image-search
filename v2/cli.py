7a660d11df2d2035311fb36e76eff1f72451b642
tikendraw <tikendraksahu1029@gmail.com>
2024-10-18 21:47:59 +0530

    added embed update fucntion,  shows image

--- v2/cli.py
+++ v2/cli.py
@@ -4,47 +4,60 @@ from PIL import Image
 import os
 import tempfile
 import json
-
+from .utils import show_image_in_terminal
 from v2.embed_model import EfficientNetEmbeddingFunction
 from v2.embedding_store import EmbeddingStore
-
+from functools import cache
 try:
     with open("config.json", "r") as f:
         config = json.load(f)
 except FileNotFoundError:
     config = {}
-
+    
+@cache
+def load_embed_store():
+    embedding_model = EfficientNetEmbeddingFunction()
+    db = EmbeddingStore("local_embeddings", embedding_model)
+    return db
 
 @click.group()
 def cli_func():
     pass
 
-
 @click.command()
-@click.option('--image_paths', '-i', type=click.Path(exists=True), multiple=True, help='Path to the image file')
+@click.argument('image_paths', type=click.Path(exists=True), nargs=-1 )
 @click.option('--num_results', '-n', default=config.get("num_similar_images", 5), help='Number of similar images to retrieve')
-def search(image_paths, num_results):
+@click.option('--show_image', type=click.BOOL, default=True, help='Show the main image')
+def search(image_paths, num_results, show_image):
     """
     Performs a reverse image search.
     """
-    embedding_model = EfficientNetEmbeddingFunction()
-    db = EmbeddingStore("local_embeddings", embedding_model)
+    db = load_embed_store()
+
 
     if not db.collection.count() > 0:
         click.echo("Error: No embeddings found. Please create embeddings first.")
         return
-
+    
     if image_paths:
 
         result = db.get_n_similar_images(image_paths, k=num_results)
 
-
-        click.echo("Similar images:")
         for i, similar_image_paths in enumerate(result["uris"]):
-            click.echo(f"For image '{image_paths[i]}':")
-            for similar_image_path in similar_image_paths:
-                click.echo(f"- {similar_image_path}")
+            image_path = str(Path(image_paths[i]).absolute())
 
+            clickable_link =  f"file://{image_path}"
+            click.echo(f"Main image: {click.style(clickable_link, fg='blue', underline=True)}")
+
+            if show_image:
+                show_image_in_terminal(image_path)
+            
+            click.echo(f"Similar images:")
+            for i, similar_image_path in enumerate(similar_image_paths,1):
+                clickable_link =  f"file://{similar_image_path}"
+                click.echo(f"{i}.  {click.style(clickable_link, fg='blue', underline=True)}")
+            
+            print("\n","-"*3, "\n")
     else:
         click.echo("Please provide at least one image path using the '-i' option.")
 
@@ -56,15 +69,13 @@ def embed():
     """
     pass
 
-
 @embed.command()
 @click.option('--dir_path', '-d', type=click.Path(exists=True), required=True, help='Path to the directory containing images')
 def create(dir_path):
     """
     Creates embeddings for images in a directory.
     """
-    embedding_model = EfficientNetEmbeddingFunction()
-    db = EmbeddingStore("local_embeddings", embedding_model)
+    db = load_embed_store()
 
     try:
         with open("config.json", "r") as f:
@@ -73,6 +84,7 @@ def create(dir_path):
         config = {"folders_embedded": []}
 
     if dir_path not in config["folders_embedded"]:
+        
         db.add_images(image_dir=dir_path)
         config["folders_embedded"].append(dir_path)
 
@@ -83,15 +95,13 @@ def create(dir_path):
     else:
         click.echo(f"Embeddings already exist for '{dir_path}'. Use 'embed update' to update them.")
 
-
 @embed.command()
 @click.option('--dir_path', '-d', type=click.Path(exists=True), required=True, help='Path to the directory containing images')
 def update(dir_path):
     """
     Updates embeddings for images in a directory.
     """
-    embedding_model = EfficientNetEmbeddingFunction()
-    db = EmbeddingStore("local_embeddings", embedding_model)
+    db = load_embed_store()
 
     try:
         with open("config.json", "r") as f:
@@ -105,15 +115,13 @@ def update(dir_path):
     else:
         click.echo(f"Embeddings do not exist for '{dir_path}'. Use 'embed create' to create them.")
 
-
 @embed.command()
 @click.option('--dir_path', '-d', type=click.Path(exists=True), required=True, help='Path to the directory containing images')
 def delete(dir_path):
     """
     Deletes embeddings for images in a directory.
     """
-    embedding_model = EfficientNetEmbeddingFunction()
-    db = EmbeddingStore("local_embeddings", embedding_model)
+    db = load_embed_store()
 
     try:
         with open("config.json", "r") as f:
@@ -122,13 +130,25 @@ def delete(dir_path):
         config = {"folders_embedded": []}
 
     if dir_path in config["folders_embedded"]:
-        # TODO: Implement logic to delete embeddings for a specific directory.
-        # This might involve iterating through the image cache and deleting
-        # corresponding entries from the ChromaDB collection.
-        click.echo(f"Deleting embeddings for '{dir_path}' is not yet implemented.")
+        
+        if not str(dir_path).lower().strip()=='delete_all_embeddings':
+            db.delete_embeddings(image_dir=dir_path)
+            
+            # update the config
+            config["folders_embedded"].remove(dir_path)
+            with open("config.json", "w") as f:
+                json.dump(config, f)
+        else:
+            db.delete_collection()
+            
+            # update the config
+            config["folders_embedded"] = []
+            with open("config.json", "w") as f:
+                json.dump(config, f)
+                
+        click.echo(f"Deleting embeddings for '{dir_path}'!")
     else:
         click.echo(f"Embeddings do not exist for '{dir_path}'.")
-
 
 @click.command()
 def show_embedded_folders():
@@ -148,7 +168,6 @@ def show_embedded_folders():
     else:
         click.echo("No folders have been embedded yet.")
 
-
 @click.command()
 def show_configs():
     """
@@ -161,7 +180,6 @@ def show_configs():
         config = {}
 
     click.echo(json.dumps(config, indent=4))
-
 
 @click.command()
 @click.option('--batch_size', '-b', type=int, default=config.get("batch_size"), help='Batch size for creating embeddings')

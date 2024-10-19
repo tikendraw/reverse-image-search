7539d8a11a74bdd6b3e97ffd78ab47ab9ba0f367
tikendraw <tikendraksahu1029@gmail.com>
2024-10-19 22:29:12 +0530

    added: list_items_in_dir

--- v2/utils.py
+++ v2/utils.py
@@ -4,6 +4,51 @@ from PIL import Image
 import tempfile
 import os
 from pathlib import Path
+from typing import List
+from pathlib import Path
+from typing import List, Tuple, Optional
+
+# Define default image extensions
+IMAGE_EXTENSIONS = (".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff")
+
+
+def list_items_in_dir(
+    dir: str,
+    include_extensions: Optional[Tuple[str]] = IMAGE_EXTENSIONS,
+    exclude_extensions: Optional[Tuple[str]] = None,
+    recursive: bool = True,
+) -> List[str]:
+    """Return a list of item paths in a directory with valid extensions.
+
+    Args:
+        dir (str): The directory to search.
+        include_extensions (tuple): File extensions to include (default: IMAGE_EXTENSIONS).
+        exclude_extensions (tuple): File extensions to exclude (default: None).
+        recursive (bool): Whether to search recursively (default: True).
+
+    Returns:
+        List[str]: List of file paths.
+    """
+
+    if not os.path.isdir(dir):
+        raise ValueError(f"Invalid directory path: {dir}")
+
+    # Set the glob pattern based on the recursive flag
+    pattern = "**/*" if recursive else "*"
+
+    # Ensure include and exclude extensions are in lowercase for comparison
+    include_extensions = tuple(ext.lower() for ext in include_extensions)
+    exclude_extensions = (
+        tuple(ext.lower() for ext in exclude_extensions) if exclude_extensions else ()
+    )
+
+    return [
+        str(p.absolute())
+        for p in Path(dir).glob(pattern)
+        if p.is_file()
+        and p.suffix.lower() in include_extensions
+        and p.suffix.lower() not in exclude_extensions
+    ]
 
 
 def show_image_in_terminal(image_path):
@@ -26,27 +71,25 @@ def show_image_in_terminal(image_path):
     # Create config
     config = chafa.CanvasConfig()
 
-    config.height = 30 #image.height//5
-    config.width  = 70 #image.width//5
+    config.height = 30
+    config.width = 70
 
     # Create canvas
     canvas = chafa.Canvas(config)
-
 
     # Draw to the canvas
     canvas.draw_all_pixels(
         image.pixel_type,
         image.get_pixels(),
-        image.width, image.height,
-        image.rowstride
+        image.width,
+        image.height,
+        image.rowstride,
     )
 
     # print output
     output = canvas.print().decode()
 
     print(output)
-
-
 
     # Delete the temporary file
     os.remove(temp_filename)

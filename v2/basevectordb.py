9b3412774be6c8d90106edb20e396daeec1798b7
tikendraw <tikendraksahu1029@gmail.com>
2024-12-21 19:15:45 +0530

    changed method

--- v2/basevectordb.py
+++ v2/basevectordb.py
@@ -5,9 +5,10 @@ from pathlib import Path
 class BaseVectorDB(ABC):
 
     @abstractmethod
-    def get_n_similar_images(self, x: list[float], k: int = 5) -> list[float]:
+    def get_similar_images(self, x: list[float], k: int = 5) -> list[float]:
         pass
 
     @abstractmethod
-    def add_images(self, images: list) -> None:
+    def update_images(self, images: list) -> None:
         pass
+    

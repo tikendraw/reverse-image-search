b0c1ae1f96e6e17ce00a5b39e0b0815b42e40392
tikendraw <tikendraksahu1029@gmail.com>
2025-01-31 09:00:37 +0530

    made difference functions for cli and web app

--- v2/common.py
+++ v2/common.py
@@ -13,7 +13,6 @@ from v2.config import load_config as l_config
 from v2.embed_model import EfficientNetEmbeddingFunction
 from v2.embedding_store import EmbeddingStore, list_images
 
-from .utils import list_items_in_dir
 
 INCLUDE_IMAGE_EXTENSIONS = (".png", ".jpg", ".jpeg", )
 
@@ -24,7 +23,7 @@ def load_embed_store():
     db = EmbeddingStore(save_dir=str(EMBEDDING_DIR.absolute()), embedding_model=embedding_model)
     return db
 
-def create_embeddings(db:EmbeddingStore, image_dir, recursive, config) -> list[str]:
+def create_embeddings_st(db:EmbeddingStore, image_dir, recursive, config) -> list[str]:
     '''
     Creates embeddings for images in a directory.
     return list of directories where images are (to update the config)
@@ -36,44 +35,70 @@ def create_embeddings(db:EmbeddingStore, image_dir, recursive, config) -> list[s
         batch_size = config.get("batch_size", 16) 
         num_batches = (len(image_paths) + batch_size - 1) // batch_size
 
-        if "streamlit" in str(st.__path__):
-            progress_bar = st.progress(0)
-        else:
-            progress_bar = None
+        progress_bar = st.progress(0)
 
         for i in tqdm(range(num_batches), desc="Creating Embeddings"):
             start = i * batch_size
             end = min((i + 1) * batch_size, len(image_paths))
             batch_paths = image_paths[start:end]
             db.update_images(image_paths=batch_paths)
-            if progress_bar:
-                progress_bar.progress((i + 1) / num_batches)
+            progress_bar.progress((i + 1) / num_batches)
+            
+    embedded_dirs = [str(Path(i).parent.absolute()) for i in image_paths]
+    return list(set(embedded_dirs))
+
+def create_embeddings_cli(db:EmbeddingStore, image_dir, recursive, config) -> list[str]:
+    '''
+    Creates embeddings for images in a directory.
+    return list of directories where images are (to update the config)
+    '''
+    if image_dir not in config["folders_embedded"]:
+        image_paths = list_images(image_dir, recursive=recursive)
+        
+        # Batching logic (if needed)
+        batch_size = config.get("batch_size", 16) 
+        num_batches = (len(image_paths) + batch_size - 1) // batch_size
 
 
-        if not "streamlit" in str(st.__path__):
-            click.echo(f"Embeddings created for images in '{image_dir}' and config updated.")
+        for i in tqdm(range(num_batches), desc="Creating Embeddings"):
+            start = i * batch_size
+            end = min((i + 1) * batch_size, len(image_paths))
+            batch_paths = image_paths[start:end]
+            db.update_images(image_paths=batch_paths)
+
+
+        click.echo(f"Embeddings created for images in '{image_dir}' and config updated.")
     else:
         image_paths=[]
-        if not "streamlit" in str(st.__path__):
-            click.echo(f"Embeddings already exist for '{image_dir}'. Use 'embed update' to update them.")
+        click.echo(f"Embeddings already exist for '{image_dir}'. Use 'embed update' to update them.")
             
     embedded_dirs = [str(Path(i).parent.absolute()) for i in image_paths]
     return list(set(embedded_dirs))
 
 
 
-def update_embeddings(db:EmbeddingStore, dir_path, recursive, config) -> list[str]:
+def update_embeddings_st(db:EmbeddingStore, dir_path, recursive, config) -> list[str]:
     ''' updates embeddings for images in a directory
     return list of directories where images are (to update the config)
     '''
     if dir_path in config["folders_embedded"]:
         db.update_images(dir_path=dir_path, recursive=recursive)
-        if not "streamlit" in str(st.__path__):
-            click.echo(f"Embeddings updated for images in '{dir_path}'.")
+    
+    # Get all image paths in directory to update config
+    image_paths = list_images(dir_path, recursive=recursive)
+    embedded_dirs = [str(Path(p).parent.absolute()) for p in image_paths]
+    return list(set(embedded_dirs))
+
+def update_embeddings_cli(db:EmbeddingStore, dir_path, recursive, config) -> list[str]:
+    ''' updates embeddings for images in a directory
+    return list of directories where images are (to update the config)
+    '''
+    if dir_path in config["folders_embedded"]:
+        db.update_images(dir_path=dir_path, recursive=recursive)
+        click.echo(f"Embeddings updated for images in '{dir_path}'.")
             
     else:
-        if not "streamlit" in str(st.__path__):
-            click.echo(f"Embeddings do not exist for '{dir_path}'. Use 'embed create' to create them.")
+        click.echo(f"Embeddings do not exist for '{dir_path}'. Use 'embed create' to create them.")
     
     # Get all image paths in directory to update config
     image_paths = list_images(dir_path, recursive=recursive)
@@ -100,10 +125,8 @@ def delete_embeddings(db:EmbeddingStore, dir_path, recursive, config) -> list[st
     return list(set(embedded_dirs))
 
 
-def get_similar_images(db:EmbeddingStore, image_paths, num_results):
+def get_similar_images_st(db:EmbeddingStore, image_paths, num_results):
     if not db.collection.count() > 0:
-        if not "streamlit" in str(st.__path__):
-            click.echo("Error: No embeddings found. Please create embeddings first.")
         return None
 
     if image_paths:
@@ -113,9 +136,23 @@ def get_similar_images(db:EmbeddingStore, image_paths, num_results):
             logging.error("Failed to generate Embeddings.")
             return {}
     else:
-        if not "streamlit" in str(st.__path__):
-            click.echo("Please provide at least one image path.")
         return None
+
+def get_similar_images_cli(db:EmbeddingStore, image_paths, num_results):
+    if not db.collection.count() > 0:
+        click.echo("Error: No embeddings found. Please create embeddings first.")
+        return None
+
+    if image_paths:
+        try: 
+            return db.get_similar_images(image_paths, k=num_results)
+        except ValueError:
+            logging.error("Failed to generate Embeddings.")
+            return {}
+    else:
+        click.echo("Please provide at least one image path.")
+        return None
+
 
 def load_config()-> dict:
     return l_config(config_file)
